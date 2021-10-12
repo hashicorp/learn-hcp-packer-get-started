d9c45f5e247f45eed3150310a390b4ae3778f502
Tu Nguyen <im2nguyen@gmail.com>
2021-10-11 18:52:48 -0700

    Update tf-application tutorial to pull from application bucket

--- tf-application/main.tf
+++ tf-application/main.tf
@@ -5,20 +5,20 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = "learn-packer-ubuntu"
+data "hcp_packer_iteration" "application" {
+  bucket_name = "learn-packer-application"
   channel     = "production"
 }
 
-data "hcp_packer_image" "ubuntu_us_west_2" {
-  bucket_name    = "learn-packer-ubuntu"
+data "hcp_packer_image" "application_us_west_2" {
+  bucket_name    = "learn-packer-application"
   cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
+  iteration_id   = data.hcp_packer_iteration.application.ulid
   region         = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.hcp_packer_image.ubuntu_us_west_2.cloud_image_id
+  ami           = data.hcp_packer_image.application_us_west_2.cloud_image_id
   instance_type = "t2.micro"
   tags = {
     Name = "Learn-HCP-Packer"
