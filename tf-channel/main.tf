c08a0582fcba4e5a84c545a0c3b469adf22b0915
Brian McClain <brianmmcclain@gmail.com>
2024-01-12 16:22:17 -0500

    Update terraform configuration with new nomenclature

--- tf-channel/main.tf
+++ tf-channel/main.tf
@@ -4,20 +4,20 @@ provider "aws" {
   region = var.region
 }
 
-data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = "learn-packer-ubuntu"
-  channel     = "production"
+data "hcp_packer_version" "ubuntu" {
+  bucket_name  = "learn-packer-ubuntu"
+  channel_name = "production"
 }
 
-data "hcp_packer_image" "ubuntu_us_east_2" {
-  bucket_name    = "learn-packer-ubuntu"
-  cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
-  region         = "us-east-2"
+data "hcp_packer_artifact" "ubuntu_us_east_2" {
+  bucket_name         = "learn-packer-ubuntu"
+  platform            = "aws"
+  version_fingerprint = data.hcp_packer_version.ubuntu.fingerprint
+  region              = "us-east-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
+  ami           = data.hcp_packer_image.ubuntu_us_east_2.external_identifier
   instance_type = "t2.micro"
   tags = {
     Name = "Learn-HCP-Packer"
